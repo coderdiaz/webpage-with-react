e514fb5a77e27a1af0f348731f657d17f59b6a7d
Javier Diaz <hello@coderdiaz.me>
2019-08-17 14:04:16 -0500

    fix: contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,6 +4,11 @@ const Contact = () => (
     <>
         <div className="container">
             <h1>Contact</h1>
+            <label>Email</label>
+            <input name="email" type="email" />
+            <label>Password</label>
+            <input name="password" type="password" />
+            <button>Enviar</button>
         </div>
     </>
 );
